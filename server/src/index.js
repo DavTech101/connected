aaea87ac2bbe44f85e41f12a159dc20eb13cae52
DavTech <dav-tech@outlook.com>
2024-04-23 21:53:52 +0200

    Bug: 🐛 Deployment vercel

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Express on Vercel'));
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+app.listen(5000, () => console.log('Server ready on port 3000.'));
 
 module.exports = app;

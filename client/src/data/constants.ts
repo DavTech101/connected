d82783b9952a3bd2d4666388417d7917775aa615
DavTech <dav-tech@outlook.com>
2024-04-26 13:29:10 +0200

    Feat: ✨ Use env for api url

--- client/src/data/constants.ts
+++ client/src/data/constants.ts
@@ -1,6 +1,6 @@
 //##########################################################################################
 // API CONFIG
 //##########################################################################################
-export const API_URL = "https://connected-phi.vercel.app";
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 export const TREE_DATA_URL = `${API_URL}/api/treedata`;
